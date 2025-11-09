1ab8a2de74f356c16c1a9caf4075d71b28fdc40a
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-09 19:17:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
-App.jsx
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import DashboardAdmin from "./pages/DashboardAdmin";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <Routes>
+      <Route path="/" element={user ? <DashboardAdmin /> : <Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;

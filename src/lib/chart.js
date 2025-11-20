5ea7d75fa5ab16b5a264e1fe7c5a4146082e984c
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-20 16:11:49 +0700

    Add chart.js to register Chart.js components

---
+++ src/lib/chart.js
@@ -0,0 +1,27 @@
+// src/lib/chart.js
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// 👇 WAJIB — daftar komponen chart yang digunakan
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default ChartJS;

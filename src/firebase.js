d8c8667569675e023587198060ff988984974bd7
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-22 21:43:02 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
-  storageBucket: "assistenku-8ef85.appspot.com",
+  storageBucket: "assistenku-8ef85.firebasestorage.app",
   messagingSenderId: "320243806907",
   appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
   measurementId: "G-ZKVLZGE552"
@@ -16,16 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Firestore
-export const db = getFirestore(app);
-
-// Auth
 export const auth = getAuth(app);
-
-// Storage
-export const storage = getStorage(app);
-
-// Analytics
-isSupported().then((v) => v && getAnalytics(app));
-
-export default app;
+export const db = getFirestore(app);

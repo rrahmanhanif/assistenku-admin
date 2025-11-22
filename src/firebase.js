842f28c90ae815fb228446e2b0179f6efdd8512f
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-22 21:13:55 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -9,13 +8,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
-  storageBucket: "assistenku-8ef85.appspot.com",   // ✔️ diperbaiki
+  storageBucket: "assistenku-8ef85.appspot.com",
   messagingSenderId: "320243806907",
   appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
   measurementId: "G-ZKVLZGE552"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
 
 // Firestore
@@ -27,9 +25,7 @@ export const auth = getAuth(app);
 // Storage
 export const storage = getStorage(app);
 
-// Analytics (hanya aktif di https)
-isSupported().then((yes) => {
-  if (yes) getAnalytics(app);
-});
+// Analytics
+isSupported().then((v) => v && getAnalytics(app));
 
 export default app;

902ba28fc662d7768c14cda3df03d8a8bd5d35d0
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-23 21:16:18 +0700

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,18 +1,7 @@
 export default function Topbar() {
   return (
-    <div className="h-16 bg-white shadow flex items-center justify-between px-6">
-      <h2 className="text-lg font-semibold text-gray-700">
-        Admin Panel
-      </h2>
-
-      <div className="flex items-center gap-3">
-        <img
-          src="/admin.png"
-          className="w-10 h-10 rounded-full"
-          alt="admin"
-        />
-        <span className="font-medium">Admin</span>
-      </div>
+    <div className="w-full h-16 bg-white shadow flex items-center justify-end px-6 ml-64">
+      <p className="text-gray-600">Admin Panel</p>
     </div>
   );
 }

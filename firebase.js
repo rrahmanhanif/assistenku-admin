dedd0cf4fe6a9edaa14bf4ce6407e99e93d75f57
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-24 08:40:23 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  projectId: "assistenku-8ef85",
+  storageBucket: "assistenku-8ef85.appspot.com",
+  messagingSenderId: "173075810921",
+  appId: "1:173075810921:web:XXXXXXXXXXXX",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);

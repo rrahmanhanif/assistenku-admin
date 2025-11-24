42718f974f956084457913f65b0b580b97b73b1e
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-24 09:11:59 +0700

    Create firebase.js

---
+++ src/core/firebase.js
@@ -0,0 +1,23 @@
+// src/core/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  projectId: "assistenku-8ef85",
+  storageBucket: "assistenku-8ef85.firebasestorage.app",
+  messagingSenderId: "320243806907",
+  appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
+  measurementId: "G-ZKVLZGE552",
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);

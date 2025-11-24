9c0f6a51b9cd5f9385d90722b4938c72e0834072
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-24 14:15:19 +0700

    Update firebase.js

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -1,3 +1,5 @@
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -7,14 +9,16 @@ const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
-  storageBucket: "assistenku-8ef85.firebasestorage.app",
+  storageBucket: "assistenku-8ef85.appspot.com",
   messagingSenderId: "320243806907",
   appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
   measurementId: "G-ZKVLZGE552",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);

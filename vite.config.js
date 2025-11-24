618f41c9999efe433dce86d1fb206ec2ba6113df
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-24 22:50:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico"],
-      workbox: {
-        globPatterns: ["**/*.{js,css,html}"],
-      },
-      manifest: {
-        name: "Assistenku Admin",
-        short_name: "Assistenku",
-        description: "Assistenku - Admin Panel",
-        theme_color: "#1d4ed8",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });

be3500451978599bbbbe474b190d45e347e1c374
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 14:26:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// config aman untuk Vercel
 export default defineConfig({
   plugins: [react()],
 });

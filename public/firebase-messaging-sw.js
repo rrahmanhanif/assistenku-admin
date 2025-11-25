4d4eb84958f37c3512871ce0e11e6b3ef2ff7012
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 16:14:14 +0700

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  projectId: "assistenku-8ef85",
+  messagingSenderId: "320243806907",
+  appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icons/icon-192.png",
+  });
+});

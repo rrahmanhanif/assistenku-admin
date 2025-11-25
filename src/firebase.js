a9fac0ab1c2ffd496aab4d3f4202576b17d120b6
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 13:53:25 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
-  databaseURL: "https://assistenku-8ef85-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "assistenku-8ef85",
-  storageBucket: "assistenku-8ef85.firebasestorage.app",
-  messagingSenderId: "320243806907",
-  appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
-  measurementId: "G-ZKVLZGE552"
+  storageBucket: "assistenku-8ef85.appspot.com",
+  messagingSenderId: "218151871662",
+  appId: "1:218151871662:web:31d0cd2a2c7f60a9a3e2de",
+  measurementId: "G-H8JE3V8W8K"
 };
 
-// Inisialisasi Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Fitur Firebase yang dipakai aplikasi
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const rtdb = getDatabase(app);
 export const storage = getStorage(app);

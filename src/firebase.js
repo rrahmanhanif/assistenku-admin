b3adcd08fcdc9693110ded117abb05e11fbaf95b
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 14:28:09 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,17 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
   storageBucket: "assistenku-8ef85.appspot.com",
-  messagingSenderId: "218151871662",
-  appId: "1:218151871662:web:31d0cd2a2c7f60a9a3e2de",
-  measurementId: "G-H8JE3V8W8K"
+  messagingSenderId: "742556046497",
+  appId: "1:742556046497:web:e4c43f4d9ab07fad62bd97"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);

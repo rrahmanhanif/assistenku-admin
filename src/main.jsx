b92e3f80bc13502f69327ca0c1c90cdbab96d721
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 12:06:48 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CoreApp from "./CoreApp";
+import AppRoot from "./AppRoot.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CoreApp />
+    <AppRoot />
   </React.StrictMode>
 );

cebe9c177367b506803f50380d117346f11dc37e
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-25 12:56:37 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
 // src/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
 
-// Konfigurasi Firebase Client SDK
+// Gunakan env dari Vercel
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_WEB_API_KEY || "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  apiKey: import.meta.env.VITE_FIREBASE_WEB_API_KEY,
   authDomain: "assistenku-8ef85.firebaseapp.com",
   databaseURL: "https://assistenku-8ef85-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "assistenku-8ef85",
@@ -18,12 +16,8 @@ const firebaseConfig = {
   measurementId: "G-ZKVLZGE552"
 };
 
-// Init
 const app = initializeApp(firebaseConfig);
 
-// Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const rtdb = getDatabase(app);      // jika ingin realtime database
-export const storage = getStorage(app);    // jika ingin upload foto/video
-export default app;
+export const rtdb = getDatabase(app);

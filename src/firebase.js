d38bbaf1f9f1512025fed76e579bcebaacedaa7c
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 11:49:42 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -26,17 +26,22 @@ mitra_profiles/
     email: string
     available: boolean
 
+// Import Firebase modules
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// ✔ Ganti konfigurasi dengan milik kamu
 const firebaseConfig = {
   apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
   authDomain: "assistenku-8ef85.firebaseapp.com",
   projectId: "assistenku-8ef85",
   storageBucket: "assistenku-8ef85.appspot.com",
   messagingSenderId: "119530003693",
-  appId: "1:119530003693:web:xxxxx",
+  appId: "1:119530003693:web:xxxxxxxxxxxxxxxx",
 };
 
+// **Initialize Firebase App**
 const app = initializeApp(firebaseConfig);
+
+// **Firestore (Database)**
 export const db = getFirestore(app);

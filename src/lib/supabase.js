2e35c9491a86d4126209b8cb6b41d0c626d6a3e5
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-07 14:57:08 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,11 @@
+// src/lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("❌ Supabase ENV tidak ditemukan di Vercel!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

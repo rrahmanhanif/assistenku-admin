73f155982eda6555951000713c0f4172a6370a7c
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-28 10:55:39 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 
 // WAJIB: import sesuai case folder kamu
-import Login from "./pages/Login";
+import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<AdminLogin />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>

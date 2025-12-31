31f04254493563f5ed7a2fe31cf962b89efd7aad
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-01 01:01:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-// src/App.jsx
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
-import Orders from "./pages/Orders";
-import OrderDetail from "./pages/OrderDetail";
 import AdminRoute from "./components/AdminRoute";
 
 export default function App() {
-  const loggedIn = localStorage.getItem("admin_auth") === "true";
-
   return (
     <Routes>
-      {/* LOGIN ADMIN */}
-      <Route path="/admin-login" element={<AdminLogin />} />
-
-      {/* DASHBOARD */}
+      <Route path="/" element={<AdminLogin />} />
       <Route
         path="/dashboard"
         element={
@@ -25,32 +16,7 @@ export default function App() {
           </AdminRoute>
         }
       />
-
-      {/* ORDERS */}
-      <Route
-        path="/orders"
-        element={
-          <AdminRoute>
-            <Orders />
-          </AdminRoute>
-        }
-      />
-
-      {/* DETAIL ORDER */}
-      <Route
-        path="/orders/:id"
-        element={
-          <AdminRoute>
-            <OrderDetail />
-          </AdminRoute>
-        }
-      />
-
-      {/* DEFAULT REDIRECT */}
-      <Route
-        path="/"
-        element={<Navigate to={loggedIn ? "/dashboard" : "/admin-login"} />}
-      />
+      <Route path="*" element={<AdminLogin />} />
     </Routes>
   );
 }

8c36d29fcd5459f1c6292f6eb175dafb1e74afe5
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-01 01:26:27 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { registerServiceWorker } from "./pwa.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-registerServiceWorker();
+import "./index.css";
+import { registerSW } from "./pwa/registerSW.js";
+
+import AdminLogin from "./pages/AdminLogin.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Portal from "./pages/Portal.jsx";
+import RequireAdmin from "./components/RequireAdmin.jsx";
+
+registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AdminLogin />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAdmin>
+            <Dashboard />
+          </RequireAdmin>
+        }
+      />
+
+      <Route
+        path="/portal"
+        element={
+          <RequireAdmin>
+            <Portal />
+          </RequireAdmin>
+        }
+      />
+
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
+  </BrowserRouter>
 );

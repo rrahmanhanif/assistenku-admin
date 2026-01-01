ceb912c312fe8ba3cd344ad0907769913a69ba7b
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-01 11:33:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      external: [
-        "@supabase/supabase-js",
-        "firebase",
-        "firebase/app",
-        "firebase/auth",
-      ],
-    },
-  },
 });

9b9b9224f8c1e1794da50649150eb6c38e622e2a
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-01 11:36:47 +0700

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Web Config ini BUKAN secret. Isi dari Firebase Console > Project settings > Your apps (Web app).
+window.FIREBASE_CONFIG = {
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  databaseURL: "https://assistenku-8ef85-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "assistenku-8ef85",
+  storageBucket: "assistenku-8ef85.firebasestorage.app",
+  messagingSenderId: "320243806907",
+  appId: "1:320243806907:web:50ecedb9a20063d7ee2f9e",
+  measurementId: "G-ZKVLZGE552"
+};

cb7cef8e056775b96e51c1117bf44ef91cb4257a
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-03 19:27:32 +0700

    Create runtime-config.js

---
+++ public/apps/admin/assets/runtime-config.js
@@ -0,0 +1,7 @@
+window.RUNTIME_CONFIG = {
+  appName: "Assistenku Admin",
+  adminCodeDefault:
+    typeof window.ADMIN_LOGIN_CODE !== "undefined"
+      ? String(window.ADMIN_LOGIN_CODE)
+      : "309309",
+};
